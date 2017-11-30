f12973a925376b8b55e4eac02837899b9a1dcd6d
raul <rauljhonatan.tola@gmail.com>
2017-11-30 12:02:02 -0500

    project created..

---
+++ src/app/index.constants.js
@@ -0,0 +1,10 @@
+/* global malarkey:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('examentDtFront')
+    .constant('malarkey', malarkey)
+    .constant('moment', moment);
+
+})();
